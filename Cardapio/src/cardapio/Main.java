c66180dea47057d7e8e29abaf398b87659d11666
Walissongs <86925855+Walissongs@users.noreply.github.com>
2023-08-14 13:58:11 -0300

    Update Main.java

--- Cardapio/src/cardapio/Main.java
+++ Cardapio/src/cardapio/Main.java
@@ -1,12 +1,19 @@
-package cardapio;
+// Arquivo: Main.java
+// Este é o ponto de entrada da aplicação.
 
+// Importa a classe SwingUtilities, que fornece utilitários para trabalhar com a interface gráfica Swing.
 import javax.swing.SwingUtilities;
+
+// Importa a classe RestaurantAPP do pacote cardapio.
 import cardapio.RestaurantAPP;
 
+// Classe Main contendo o método main que inicia a aplicação.
 public class Main {
     public static void main(String[] args) {
-           SwingUtilities.invokeLater(new Runnable() {
+        // O método SwingUtilities.invokeLater é usado para garantir que a criação da interface gráfica ocorra na thread de eventos Swing.
+        SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                // Cria uma nova instância da classe RestaurantAPP, iniciando a aplicação.
                 new RestaurantAPP();
             }
         });
