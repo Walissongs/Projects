f8271652b034ff1a1b171a3ef73c352467087566
Walissongs <86925855+Walissongs@users.noreply.github.com>
2023-08-14 14:19:09 -0300

    Update ItemType.java

--- Cardapio/src/cardapio/ItemType.java
+++ Cardapio/src/cardapio/ItemType.java
@@ -1,12 +1,14 @@
 
+/*
+ * Pacote onde a classe ItemType está localizada.
+ */
 package cardapio;
 
 /**
- *
- * @author walis
+ * Enumeração ItemType define os tipos possíveis de itens no cardápio.
+ * Pode ser usado para categorizar itens como comida ou bebida.
  */
-
-
 public enum ItemType {
-    COMIDA, BEBIDA
+    COMIDA, // Representa o tipo de item como comida.
+    BEBIDA  // Representa o tipo de item como bebida.
 }
