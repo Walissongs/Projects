7e3c4ae4d11e37ef7f9af8e1dbe71329f85e9a48
Walissongs <86925855+Walissongs@users.noreply.github.com>
2023-08-14 13:58:36 -0300

    Update Menu.java

--- Cardapio/src/cardapio/Menu.java
+++ Cardapio/src/cardapio/Menu.java
@@ -1,6 +1,5 @@
-
-package cardapio;
-
+// Arquivo: Menu.java
+// Esta classe representa o menu diário do restaurante.
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,23 +7,29 @@ import java.util.List;
 import java.util.Map;
 
 public class Menu {
+    // Mapa que associa cada dia da semana a uma lista de itens do menu.
     private Map<String, List<MenuItem>> menuItems;
 
+    // Construtor da classe Menu.
     public Menu() {
+        // Inicializa o mapa e preenche com listas vazias para cada dia da semana.
         menuItems = new HashMap<>();
         for (int i = 2; i <= 6; i++) {
             menuItems.put(getDayName(i), new ArrayList<>());
         }
     }
 
+    // Adiciona um item ao menu de um determinado dia.
     public void addMenuItem(int day, MenuItem item) {
         menuItems.get(getDayName(day)).add(item);
     }
 
+    // Obtém a lista de itens do menu para um determinado dia.
     public List<MenuItem> getMenuItems(int day) {
         return menuItems.get(getDayName(day));
     }
     
+    // Retorna o nome do dia da semana com base no número do dia.
     private String getDayName(int day) {
         switch (day) {
             case 2: return "Segunda-feira";
@@ -35,5 +40,4 @@ public class Menu {
             default: return "";
         }
     }
-
 }
