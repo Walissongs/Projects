9ba52eb1ed4303a659c228ff7ae8bee6a8ac8212
Walissongs <86925855+Walissongs@users.noreply.github.com>
2023-08-14 13:58:53 -0300

    Update MenuItem.java

--- Cardapio/src/cardapio/MenuItem.java
+++ Cardapio/src/cardapio/MenuItem.java
@@ -1,28 +1,32 @@
+// Arquivo: MenuItem.java
+// Esta classe representa um item individual no menu.
 
 package cardapio;
 
 public class MenuItem {
-    
+    // Atributos privados para o nome e o tipo do item.
     private String name;
     private ItemType type;
 
+    // Construtor da classe MenuItem.
     public MenuItem(String name, ItemType type) {
         this.name = name;
         this.type = type;
     }
 
+    // Método para obter o nome do item.
     public String getName() {
         return name;
     }
 
+    // Método para obter o tipo do item.
     public ItemType getType() {
         return type;
     }
 
+    // Sobrescrita do método toString para retornar o nome do item.
     @Override
     public String toString() {
         return name;
     }
 }
-
-
